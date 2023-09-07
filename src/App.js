03f1cf3c9a6e417c18d500a219951d7aace98b33
Akhil Kumar <akhilguretia@gmail.com>
2023-09-07 21:48:27 +0530

    Building the layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-function App() {
+const App = () => {
   return (
-    <div>
-      <p>Hello React!</p>
+    <div className="app">
+      <Logo />
+      <Form />
+      <List />
+      <Stats />
     </div>
   );
 }
+
+const Logo = () => {
+  return (
+    <h1> 🌴 SO FAR AWAY 💼 </h1>
+  );
+};
+
+const Form = () => {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip ?</h3>
+    </div>
+  );
+};
+
+const List = () => {
+  return (
+    <div className="list">
+      List
+    </div>
+  );
+};
+
+const Stats = () => {
+  return (
+    <footer className="stats">
+      <em>💼 You have X items on your list, and you already packed X (X%) </em>
+    </footer>
+  );
+};
 
 export default App;
