0f2afac8215435de151cf9041a9c4e70b0f29d12
Akhil Kumar <akhilguretia@gmail.com>
2023-09-12 03:23:10 +0530

    conditionally rendering the stats component

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () => {
       <Logo />
       <Form OnAddItem={handleAddItem} />
       <PackagingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -99,10 +99,26 @@ const Item = ({ item, onDeleteItem, onToggleItem }) => {
   );
 }
 
-const Stats = () => {
+const Stats = ({ items }) => {
+
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+  }
+
+  const numItems = items.length;
+  const numPacked = items.filter(item => item.packed).length;
+  const percentage = Math.round(numPacked / numItems * 100);
+
   return (
     <footer className="stats">
-      <em>💼 You have X items on your list, and you already packed X (X%) </em>
+      <em>
+        {percentage === 100 ? "You got everything! Let's roll ✈️" :
+          `💼 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 };
