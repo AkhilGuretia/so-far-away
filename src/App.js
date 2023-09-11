53e97a94a36100f0b476f8e8f3fa9fb67b3e7446
Akhil Kumar <akhilguretia@gmail.com>
2023-09-12 04:30:10 +0530

    adding sorting features to list

--- src/App.js
+++ src/App.js
@@ -78,13 +78,39 @@ const Form = ({ OnAddItem }) => {
 };
 
 const PackagingList = ({ items, onDeleteItem, onToggleItem }) => {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description") {
+    sortedItems = items.slice().sort(
+      (a, b) => a.description.localeCompare(b.description)
+    );
+  }
+
+  if (sortBy === "packed") {
+    sortedItems = items.slice().sort(
+      (a, b) => Number(a.packed) - Number(b.packed)
+    );
+  }
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item item={item} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} key={item.id} />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(event) => setSortBy(event.target.value)}>
+          <option value="input">Sort By Input Order</option>
+          <option value="description">Sort By Description</option>
+          <option value="packed">Sort By Packing Status</option>
+        </select>
+      </div>
     </div>
   );
 };
